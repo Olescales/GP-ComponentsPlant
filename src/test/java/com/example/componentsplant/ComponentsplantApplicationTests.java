bb4c95051b860b5c1fe86f8526859f46fcc6185a
Oleg <48734675+Olescales@users.noreply.github.com>
2020-02-09 16:35:53 +0300

    2

--- src/test/java/com/example/componentsplant/ComponentsplantApplicationTests.java
+++ src/test/java/com/example/componentsplant/ComponentsplantApplicationTests.java
@@ -23,25 +23,4 @@ class ComponentsplantApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    public void testClientSignInIsOk() throws Exception {
-        // given
-        // when
-        mockMvc.perform(post("/client/sign-up")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\n" +
-                        "  \"name\" : \"helg\",\n" +
-                        "  \"legalAddress\" : \"bagrationa street\",\n" +
-                        "  \"accountNumberOfTheTaxpayer\" : \"987654321\",\n" +
-                        "  \"country\" : \"belarus\",\n" +
-                        "  \"bankAccount\" : \"12345678901234567890\",\n" +
-                        "  \"email\" : \"me@client.com\",\n" +
-                        "  \"password\" : \"goodpassword\"\n" +
-                        "}"))
-                // then
-                .andExpect(status().isCreated())
-                .andExpect(content().json("{\n" +
-                        "  \"id\" : 1\n" +
-                        "}"));
-    }
 }
